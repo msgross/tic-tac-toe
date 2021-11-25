f25c9e2ed9092541d27872d155043f6d62e88af6
mgross <mark@grossremarks.com>
2021-11-24 20:37:44 -0800

    swapping to a bitmask instead of matrix

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-import numpy as np
-import numpy.ma as mask
 from state import State
 from state import victory_conditions
 
@@ -8,21 +6,39 @@ def eval(state: State):
     pass
 
 
+def is_victory(player_state, victory_condition):
+    mask = player_state & victory_condition
+    if mask ^ victory_condition == 0:
+        return True
+
 def is_terminal(state: State, is_depth_remaining: bool, is_time_remaining: bool):
     if not is_depth_remaining or not is_time_remaining:
         return True
-    for victory_condition in victory_conditions:
-        mask_out_p1 = mask.masked_equal(state.state, state.player1)
-        mask_out_p2 = mask.masked_equal(state.state, state.player2)
-        mask.filled(mask_out_p1, fill_value = )
+    for victory_condition in state.victory_conditions:
+        if is_victory(state.player1_state, victory_condition):
+            return True
+        elif is_victory(state.player2_state, victory_condition):
+            return True
+    return False
 
 
 def result(state: State, action):
     next_state = state.copy()
-    next_state.state[action[0], action[1]] = next_state.current_player
+    if next_state.current_player == next_state.player1:
+        next_state.player1_state = next_state.player1_state | action
+    else:
+        next_state.player2_state = next_state.player2_state | action
     next_state.current_player = next_state.next_player()
     return next_state
 
 
 def actions(state: State):
-    return np.asarray(state.state == 0).nonzero()
+
+    valid_moves = []
+    all_state = state.player1_state | state.player2_state
+    for shift in range(0,9):
+        move = 0x01 << shift
+        masked_state = all_state & move
+        if masked_state == 0:
+            valid_moves.append(move)
+    return valid_moves
--- state.py
+++ state.py
@@ -1,19 +1,19 @@
-import numpy as np
 from enum import Enum
 
 # Possible victory states
 
 
-_horiz_top = np.array([(1, 1, 1), (0, 0, 0), (0, 0, 0)])
-_horiz_center = np.array([(0, 0, 0), (1, 1, 1), (0, 0, 0)])
-_horiz_bottom = np.array([(0, 0, 0), (0, 0, 0), (1, 1, 1)])
+_horiz_top = 0x01C0   # 000111000000
+_horiz_center = 0x38  # 000000111000
+_horiz_bottom = 0x07  # 000000000111
 
-_vert_left = np.array([(1, 0, 0), (1, 0, 0), (1, 0, 0)])
-_vert_center = np.array([(0, 1, 0), (0, 1, 0), (0, 1, 0)])
-_vert_right = np.array([(0, 0, 1), (0, 0, 1), (0, 0, 1)])
+_vert_left = 0x0124   # 000100100100
+_vert_center = 0x92   # 000010010010
+_vert_right = 0x49     # 000001001001
 
-_diag_right = np.array([(1, 0, 0), (0, 1, 0), (0, 0, 1)])
-_diag_left = np.array([(0, 0, 1), (0, 1, 0), (1, 0, 0)])
+_diag_right = 0x0111  # 000100010001
+_diag_left = 0x54    # 000001010100
+
 
 victory_conditions = [_horiz_top, _horiz_center, _horiz_bottom,
                       _vert_left, _vert_center, _vert_right,
@@ -32,14 +32,17 @@ class State:
         self.player1 = 1
         self.player2 = 2
         self.current_player = self.player1
-        self.state = np.zeros([3, 3], dtype=int)
+        self.player1_state = 0x0000
+        self.player2_state = 0x0000
         self.status = Status.IN_PROGRESS
+        self.winner = None
 
     def copy(self):
         copy_state = State()
         copy_state.current_player = self.current_player
         copy_state.status = self.status
-        copy_state.state = self.state.copy()
+        copy_state.player1_state = self.player1_state.copy()
+        copy_state.player2_state = self.player2_state.copy()
         return copy_state
 
     def next_player(self):
